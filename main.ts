360a3eefdd60e417cfaf07563e56adb09d908abc
Mariam-Abugamga <71400493+Mariam-Abugamga@users.noreply.github.com>
2020-10-07 12:54:27 -0400

    The potentiometer resistance now is shown on the Micro:Bit

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
+// Created by: Mariam Abugamga
+// Created on: Oct, 7, 2020
+// This program shows the resistance of the potentiometer.
 let potentiometer = 0
 basic.clearScreen()
 basic.forever(function () {
-    potentiometer = 0
+    potentiometer = pins.analogReadPin(AnalogPin.P1)
+    basic.showNumber(potentiometer)
+    basic.pause(1000)
 })
